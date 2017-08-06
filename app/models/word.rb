fdfd4edc314a52e69e1a0928f8d4517fbf5f7767
gersonvan <gersonvan@gmail.com>
2017-08-05 21:09:52 -0300

    Campo de busca

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,14 @@
 class Word < ApplicationRecord
-end
+  require 'mw_dictionary_api'
+
+  client = MWDictionaryAPI::Client.new(ENV['MW_COLLEGIATE_API_KEY'], api_type: "collegiate")
+
+  def self.search(name)
+      result = where('name LIKE ?', "#{name}").order('name DESC')
+      if !result.to_s.empty?
+        result
+      else
+        client.search(name)
+      end
+    end
+  end
